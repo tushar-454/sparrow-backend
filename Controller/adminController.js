08e38bbd26e60a2fdd838a12482a77b70bab8b40
tushar-454 <imtushar454@gmail.com>
2024-02-27 22:26:54 +0600

    Add getAccountInfo controller to adminController.js and update admin account with total amount of admin using aggregate

--- Controller/adminController.js
+++ Controller/adminController.js
@@ -1,3 +1,4 @@
+const Account = require('../Model/Account');
 const Admin = require('../Model/Admin');
 
 /**
@@ -26,6 +27,42 @@ const getAdminInfo = async (req, res, next) => {
   }
 };
 
+/**
+ * get account info controller
+ */
+const getAccountInfo = async (req, res, next) => {
+  try {
+    const { phone } = req.params;
+    const account = await Account.findOne({ phone });
+    // get total account balance from Account model which role is not admin
+    const totalAccountBalance = await Account.aggregate([
+      { $match: { role: { $ne: 'Admin' } } },
+      { $group: { _id: null, total: { $sum: '$balance' } } },
+    ]);
+    account.userBalance = totalAccountBalance[0].total ?? account.balance;
+    await account.save();
+    res.status(200).json({
+      status: 200,
+      message: 'Account found',
+      data: account,
+    });
+    if (!account) {
+      return res.status(404).json({
+        status: 404,
+        message: 'Admin not found',
+      });
+    }
+    res.status(200).json({
+      status: 200,
+      message: 'Admin account found',
+      data: account,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
 module.exports = {
   getAdminInfo,
+  getAccountInfo,
 };
--- Routes/admin.js
+++ Routes/admin.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const adminController = require('../Controller/adminController');
 
 router.get('/info/:phone', adminController.getAdminInfo);
+router.get('/account/:phone', adminController.getAccountInfo);
 
 module.exports = router;
