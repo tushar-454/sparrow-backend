f3f663422a21d843f2d5e2adb7547c06dbf52270
tushar-454 <imtushar454@gmail.com>
2024-02-27 23:01:49 +0600

    Add user management and user request action controllers

--- Controller/adminController.js
+++ Controller/adminController.js
@@ -1,5 +1,6 @@
 const Account = require('../Model/Account');
 const Admin = require('../Model/Admin');
+const User = require('../Model/User');
 
 /**
  * get admin info controller
@@ -62,7 +63,83 @@ const getAccountInfo = async (req, res, next) => {
   }
 };
 
+/**
+ * update user info controller
+ */
+
+const userManage = async (req, res, next) => {
+  try {
+    const { phone } = req.params;
+    const { isActiveAccount } = req.body;
+    const user = await User.findOne({ phone });
+    user.isActiveAccount = isActiveAccount;
+    await user.save();
+    res.status(200).json({
+      status: 200,
+      message: 'User account updated',
+      data: user,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+/**
+ * user request admin action controller
+ */
+
+const userReauestAction = async (req, res, next) => {
+  try {
+    const { phone } = req.params;
+    const { requestType } = req.body;
+    const user = await User.findOne({ phone });
+    user[requestType] = false;
+    await user.save();
+    const userAccount = await Account.findOne({ phone });
+    switch (requestType) {
+      case 'balance':
+        userAccount.balance = userAccount.balance + 100000;
+        await userAccount.save();
+        res.status(200).json({
+          status: 200,
+          message: 'User balance updated',
+          data: userAccount,
+        });
+        break;
+      case 'withdraw':
+        if (userAccount.balance < 100000) {
+          userAccount.balance = 0;
+          await userAccount.save();
+          return res.status(200).json({
+            status: 200,
+            message: 'User balance updated',
+            data: userAccount,
+          });
+        }
+        userAccount.balance = userAccount.balance - 100000;
+        await userAccount.save();
+        res.status(200).json({
+          status: 200,
+          message: 'User balance updated',
+          data: userAccount,
+        });
+        break;
+      default:
+        res.status(200).json({
+          status: 200,
+          message: 'User balance updated',
+          data: userAccount,
+        });
+        break;
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
 module.exports = {
   getAdminInfo,
   getAccountInfo,
+  userManage,
+  userReauestAction,
 };
--- Routes/admin.js
+++ Routes/admin.js
@@ -3,5 +3,7 @@ const adminController = require('../Controller/adminController');
 
 router.get('/info/:phone', adminController.getAdminInfo);
 router.get('/account/:phone', adminController.getAccountInfo);
+router.patch('/userManage/:phone', adminController.userManage);
+router.patch('/userRequestAction/:phone', adminController.userReauestAction);
 
 module.exports = router;
